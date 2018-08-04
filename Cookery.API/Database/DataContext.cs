5e533521273f7a9f305e56fc27af774bc5bd9488
Joseph Alfredo <jalfredopoly@gmail.com>
2018-08-03 18:27:18 -0700

    Initial Commit

---
+++ Cookery.API/Database/DataContext.cs
@@ -0,0 +1,12 @@
+using Cookery.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookery.API.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+} 
